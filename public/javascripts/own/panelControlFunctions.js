c1f221d60666ade4fba2054d122a4a04c08a75f1
Daniel Arturo Rodriguez Rosas <darr.0725@gmail.com>
2013-04-25 00:11:51 -0500

    The panel exchange is working beach

--- public/javascripts/own/panelControlFunctions.js
+++ public/javascripts/own/panelControlFunctions.js
@@ -1,27 +1,16 @@
-function changeSidebarActive(){
+function changeContainerPanel(event){
+	event.preventDefault();
+	event.stopPropagation();
+	var identifier = '#'+ $(this).data('li-name');
+	$('.row-fluid').find('.container').hide();	
+	$('.row-fluid').find('.container').removeClass('hidden');
+	$('.row-fluid').find(identifier).slideDown();
+
+	//This part is for the sidebar .active change
 	$(this).closest('.sidebar').find('.active').removeClass('active');
 	$(this).addClass('active');
 }
 
-function expandRowFluid(clickedLiName){
-	var identificador = '#'+ clickedLiName;
-	console.log('entre ' + clickedLiName + identificador);
-	
-	$('.row-fluid').fadeOut(0,'swing', function(){
-		$('.container-fluid').find(identificador).removeClass('hidden');
-		$('.container-fluid').find(identificador).fadeIn();
-	});
-}
-
-function changeRowFluidPanel(event){
-	event.stopPropagation();
-	event.preventDefault();
-	var clickedLi = +$(this).data('row-code');
-	var clickedLiName = $(this).data('row-name');
-	expandRowFluid(clickedLiName);
-}
-
-$('document').ready(function(){
-	$('.sidebar').on('click','li',changeSidebarActive);
-	$('.sidebar').on('click','li',changeRowFluidPanel);
+$(document).ready(function(){
+	$('.sidebar').on('click','li',changeContainerPanel);
 })
